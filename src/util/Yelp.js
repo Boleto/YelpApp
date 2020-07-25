8eb58069a713d07b9e3b9760fb2700e913c62440
Marcos <marcosphoff@gmail.com>
2020-07-24 22:48:52 -0300

    .

---
+++ src/util/Yelp.js
@@ -0,0 +1,32 @@
+const apiKey = 'Tm_VByIoMe9gVcQfFL1CttNbfHMI_XnAjP89E7wIK4Lw3B2Z3dXhKzVUl2FaiR3lor_oUpmbgzqs0M4albO0pAUB6JAanJqduvovFzfoa-8AGSJcjstsQbmWHJ8XX3Yx';
+
+const Yelp = {
+    searchYelp(term, location, sortBy){
+        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+            headers: {
+                Authorization: `Bearer ${apiKey}`,
+            },
+        }).then((response) => {
+            return response.json();
+        }).then((jsonResponse) => {
+            if(jsonResponse.businesses) {
+                return jsonResponse.businesses.map(((business) => {
+                    return {
+                        id: business.id,
+                        imageSrc: business.image_url,
+                        name: business.name,
+                        address: business.location.address1,
+                        city: business.location.city,
+                        state: business.location.state,
+                        zipCode: business.location.zip_code,
+                        category: business.categories[0].title,
+                        rating: business.rating,
+                        reviewCount: business.review_count
+                    }
+                }));
+            }
+        })
+    }
+};
+
+export default Yelp;
